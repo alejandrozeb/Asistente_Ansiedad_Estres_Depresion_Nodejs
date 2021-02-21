be1507c384dd1ea34e761e2eda3d4d20cb1485a7
unknown <alejandrojzeballos@gmail.com>
2021-02-21 01:22:58 -0400

    {add} configurando el homecontroller

---
+++ AsistenteAED/src/controllers/home.Controller.js
@@ -0,0 +1,8 @@
+
+
+class HomeController{
+    
+    index(req,res){
+        return res.send();
+    }
+}
--- AsistenteAED/src/routes/homeRoutes.js
+++ AsistenteAED/src/routes/homeRoutes.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 const router = Router();
-
+const controllers = require("../controllers");
 router.get('/home', ()=>{console.log('hola desde home')});
 
 module.exports = router;
