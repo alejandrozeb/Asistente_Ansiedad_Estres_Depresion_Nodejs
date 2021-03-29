7bf4d9ac20cbc3dfc1dc2403f50f88b8df155527
unknown <alejandrojzeballos@gmail.com>
2021-03-28 23:55:34 -0400

    {add} añadiendo los modelos de contacto y test

---
+++ AsistenteAED/src/models/ContactoModel.js
@@ -0,0 +1,18 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const User = sequelize.define('User', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  }
+}, {
+  // Other model options go here
+});
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
