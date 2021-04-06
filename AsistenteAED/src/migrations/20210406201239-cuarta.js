8c1813cea18ea84b8742d4751c952b605448a239
unknown <alejandrojzeballos@gmail.com>
2021-04-06 16:19:01 -0400

    {add} migracion de persona y usuario

---
+++ AsistenteAED/src/migrations/20210406201239-cuarta.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      u_telefono: {
+        type: Sequelize.INTEGER
+      },
+      u_genero: {
+        type: Sequelize.STRING
+      },
+      u_nombre: {
+        type: Sequelize.STRING
+      },
+      fk_idPersona: {
+        type: Sequelize.INTEGER,
+        /* references: {
+          model: {
+            tableName: 'personas',
+            schema: 'schema'
+          },
+          key: 'id'
+        },
+        allowNull: false */
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
--- AsistenteAED/src/models/doctor.js
+++ AsistenteAED/src/models/doctor.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.Persona.hasOne(Doctor);
+      Doctor.belongsTo(models.Persona);
     }
   };
   Doctor.init({
