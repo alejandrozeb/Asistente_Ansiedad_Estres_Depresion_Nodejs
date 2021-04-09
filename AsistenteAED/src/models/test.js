ab124a57b465da54b95a91f6c29804a6c5150ac9
unknown <alejandrojzeballos@gmail.com>
2021-04-08 20:16:15 -0400

    {add} añadiendo la asociacion 1:n al modelo entre usuario y test

--- AsistenteAED/src/models/test.js
+++ AsistenteAED/src/models/test.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.Usuario.hasMany(Test);
+      Test.belongsTo(models.Usuario);
+
     }
   };
   Test.init({
