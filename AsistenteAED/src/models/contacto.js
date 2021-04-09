c3462f1d05c2f69bd73218c8b7f29afc4ad6dc1b
unknown <alejandrojzeballos@gmail.com>
2021-04-08 20:09:15 -0400

    {add} añadiendo la asociacion al modelo entre usuario y contacto

--- AsistenteAED/src/models/contacto.js
+++ AsistenteAED/src/models/contacto.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.Usuario.hasOne(contacto);
+      contacto.belongsTo(modelos.Usuario);
     }
   };
   contacto.init({
