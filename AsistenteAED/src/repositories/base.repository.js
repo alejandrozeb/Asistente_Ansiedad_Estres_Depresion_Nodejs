fa089745c0cfbc29a9a86bcb0b576312d074fbe7
unknown <alejandrojzeballos@gmail.com>
2021-04-09 23:51:03 -0400

    {add} añadiendo el repositorio de persona

--- AsistenteAED/src/repositories/base.repository.js
+++ AsistenteAED/src/repositories/base.repository.js
@@ -1,10 +1,9 @@
 class baseRepository {
-  constructor(db, entity) {
-    this._db = db;
+  constructor(entity) {
     this.entity = entity;
   }
 
   getAll(){
-      return this._db[this.entity].findAll(); 
+      return this.entity.findAll(); 
   }
 }
--- AsistenteAED/src/repositories/personaRepostitory.js
+++ AsistenteAED/src/repositories/personaRepostitory.js
@@ -1,5 +1,19 @@
 class Persona {
-
+    constructor(entity) {
+        this.entity = entity;
+      }
+    
+      getAll(){
+          return this.entity.findAll(); 
+      }
+    
+      get(id){
+        return this.entity.find
+      }
+    
+      create(){}
+    
+      update(){}
 }
 
 module.exports = Persona;
