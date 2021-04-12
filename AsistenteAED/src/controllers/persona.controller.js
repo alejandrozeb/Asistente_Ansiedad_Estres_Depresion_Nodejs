c54815cbc1348435b5a6a285ef15cdcff2ad9268
unknown <alejandrojzeballos@gmail.com>
2021-04-12 18:59:37 -0400

    {fix bug} enrutador funcionando desde home

--- AsistenteAED/src/controllers/persona.controller.js
+++ AsistenteAED/src/controllers/persona.controller.js
@@ -3,8 +3,8 @@ class PersonaController{}
 
 PersonaController.index = async (req,res)=>{
     const personas = await PersonaService.obtenerPersonas();
-    console.log('hi ' + personas); 
-    //res.send(JSON.stringify(personas));
+    //console.log('hi ' + personas); 
+    res.send(JSON.stringify(personas));
 };
 
 module.exports = PersonaController;
